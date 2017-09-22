019ea6a5c9cc162d4a8505989dbd63fbf809cd54
Ashwini <ashwini@etherlabs.io>
2017-09-22 10:43:41 +0530

    Tested this docker version in staging

--- scripts/run_janus.sh
+++ scripts/run_janus.sh
@@ -1,4 +1,4 @@
-sed -i "s@http:\/\/localhost:3050\/events@$JANUS_EVENT_LISTENER@g" /usr/local/etc/janus/janus.eventhandler.sampleevh.cfg
+sed -i "s@^backend.*@backend = $JANUS_EVENT_LISTENER@g" /usr/local/etc/janus/janus.eventhandler.sampleevh.cfg
 /etc/init.d/nginx restart
 /etc/init.d/cron restart
 janus
